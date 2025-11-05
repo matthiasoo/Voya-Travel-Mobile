3e961c2414c4ae1b41f60739500cb9ddabe65a5d
matthiasoo <makky.x.55@gmail.com>
2025-11-05 01:59:21 +0100

    Firebase set up

--- frontend/app/index.tsx
+++ frontend/app/index.tsx
@@ -1,9 +1,49 @@
-import {Text, View} from "react-native";
+import { Text, View, TextInput, Button, ActivityIndicator } from "react-native";
+import { useState } from "react";
+import auth from "@react-native-firebase/auth";
+import { FirebaseError } from "@firebase/util";
 
 export default function Index() {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [loading, setLoading] = useState(false);
+
+    const signUp = async () => {
+        setLoading(true);
+        try {
+            await auth().createUserWithEmailAndPassword(email, password);
+        } catch (e: any) {
+            const err = e as FirebaseError;
+            alert('Błąd rejestracji: ' + err.message);
+        } finally {
+            setLoading(false);
+        }
+    }
+
+    const signIn = () => {
+
+    }
+
     return (
-        <View className="flex-1 items-center justify-center bg-black">
-            <Text className="text-5xl font-thin text-white">Voya Travel</Text>
+        <View className="flex-1 gap-10 items-center justify-center">
+            <Text className="p-2 text-5xl">Voya Travel</Text>
+            <TextInput
+                className="border w-[20rem]"
+                value={email}
+                onChangeText={setEmail}
+                placeholder="E-mail"
+            />
+            <TextInput
+                className="border w-[20rem]"
+                value={password}
+                onChangeText={setPassword}
+                placeholder="Hasło"
+            />
+            {loading ? (
+                <ActivityIndicator size="small" />
+            ) : (
+                <Button onPress={signUp} title="Sign Up" />
+            )}
         </View>
     );
 }
